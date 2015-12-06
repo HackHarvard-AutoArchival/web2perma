fd0432fc227bb877e6b56957396965ae1fcfcf45
Shantanu Gupta <sg3273@drexel.edu>
2015-12-06 05:31:05 -0500

    Re-write of the code Output CSV for PDF Check memento before perma.cc
     Changes to be committed:
    	modified:   web2perma.c

--- web2perma.c
+++ web2perma.c
@@ -1,38 +1,39 @@
 /*
- * Some license I don't really care about
+ * 2perma.c
+ * Brought to you by the HackHarvard AutoArchival team.
  *
- * Seriously, this is hackathon code and
- * you shouldn't probably be using it in
- * your project. Think twice, make it
- * thrice. Most of it is very closely
- * tailored to the data crunched at the
- * event and can have corner cases that
- * weren't tested as part of the sample
- * data.
- *
- *	- Shantanu Gupta <shans95g@gmail.com>
+ * Shantanu Gupta <sg3273(AT)drexel.edu>
  */
+
 
 #include <stdio.h>
 #include <string.h>
 #include <stdlib.h>
 #include <curl/curl.h>
 
+#define MTO_API "http://timetravel.mementoweb.org/api/json/0/"
 
-/*
- * Min, but with a twist.
- * If either of the arguments is zero then return the other one.
- */
-#define MIN(a,b) (((a)<(b))? ((a) ? (a) : (b)):((b) ? (b) : (a)))
+#define PRM_KEY "4e4b3c5a831c61c77ae7343d91bcb7f548942ca0"
+#define PRM_API "https://api.perma.cc/v1/archives/?api_key=" PRM_KEY
+
+#define MIN_PTR(a, b)                                                          \
+	({                                                                         \
+		__typeof__ (a) _a = (a);                                               \
+		__typeof__ (b) _b = (b);                                               \
+		_b < _a ? _b != NULL ? _b : _a : _a != NULL ? _a : _b;                 \
+	})
+
 
 /*
  * Internal callback function to handle JSON response.
  */
-static size_t JSONCallback(void *contents, size_t size, size_t nmemb, void *userp)
+static size_t JSONCallBack(void *contents, size_t size, size_t nmemb, void *userp)
 {
 	char **ptr = userp;
-	*ptr = realloc(*ptr, (size * nmemb) + 1);
 
+	*ptr = realloc(*ptr, (size * nmemb) + 1);
+	if (*ptr == NULL)
+		exit(-1);
 	memcpy(*ptr, contents, size * nmemb);
 	(*ptr)[size * nmemb] = '\0';
 
@@ -42,166 +43,225 @@ static size_t JSONCallback(void *contents, size_t size, size_t nmemb, void *user
 /*
  * Gateway to libcurl
  */
-char *getPermaLink(char *URL)
+char *cUrlPerform(char *pURL, char *JSONReq)
 {
 	CURL *curl;
-	CURLcode res;
+	char *JSONRes = NULL;
 	struct curl_slist *headers = NULL;
-	char *JSONReq = malloc(32 + strlen(URL)), *JSONRes = NULL, *PermLnk = NULL;
 
-	curl_global_init(CURL_GLOBAL_ALL);
 	curl = curl_easy_init();
 	if (!curl)
-		return NULL;
-
-	sprintf(JSONReq, "{\n\t\"url\": \"%s\",\n\t\"title\": \"\"\n}\n", URL);
+		exit(-1);
 
 	headers = curl_slist_append(headers, "Accept: application/json");
-	headers = curl_slist_append(headers, "Content-Type: application/json");
+	if (JSONReq)
+		headers = curl_slist_append(headers, "Content-Type: application/json");
 
-	curl_easy_setopt(curl, CURLOPT_URL, "https://api.perma.cc/v1/archives/?api_key=4e4b3c5a831c61c77ae7343d91bcb7f548942ca0");
+	curl_easy_setopt(curl, CURLOPT_URL, pURL);
 	curl_easy_setopt(curl, CURLOPT_SSL_VERIFYHOST, 0);
 	curl_easy_setopt(curl, CURLOPT_SSL_VERIFYPEER, 0);
-	curl_easy_setopt(curl, CURLOPT_CUSTOMREQUEST, "POST");
+	if (JSONReq == NULL)
+		curl_easy_setopt(curl, CURLOPT_CUSTOMREQUEST, "GET");
+	else
+		curl_easy_setopt(curl, CURLOPT_CUSTOMREQUEST, "POST");
 	curl_easy_setopt(curl, CURLOPT_HTTPHEADER, headers);
-	curl_easy_setopt(curl, CURLOPT_POST, 1);
-	curl_easy_setopt(curl, CURLOPT_POSTFIELDS, JSONReq);
-	curl_easy_setopt(curl, CURLOPT_POSTFIELDSIZE, strlen(JSONReq));
-	curl_easy_setopt(curl, CURLOPT_WRITEFUNCTION, JSONCallback);
+	if (JSONReq)
+	{
+		curl_easy_setopt(curl, CURLOPT_POST, 1);
+		curl_easy_setopt(curl, CURLOPT_POSTFIELDS, JSONReq);
+		curl_easy_setopt(curl, CURLOPT_POSTFIELDSIZE, strlen(JSONReq));
+	}
+	curl_easy_setopt(curl, CURLOPT_WRITEFUNCTION, JSONCallBack);
 	curl_easy_setopt(curl, CURLOPT_WRITEDATA, (void *) &JSONRes);
 	curl_easy_setopt(curl, CURLOPT_USERAGENT, "permacc++");
-	res = curl_easy_perform(curl);
+	curl_easy_perform(curl);
+	curl_slist_free_all(headers);
 	curl_easy_cleanup(curl);
-	curl_global_cleanup();
-	free(JSONReq);
 
-	char *idx = strstr(JSONRes, "\"guid\": \"");
-	if (idx)
-	{
-		PermLnk = malloc(10);
-		idx += 9;
-		strncpy(PermLnk, idx, 9);
-		PermLnk[9] = '\0';
-	}
+	if (JSONReq)
+		free(JSONReq);
+
+	return JSONRes;
+}
+
+
+char *getMemtoLink(char *sURL)
+{
+	char *JSONEndp, *JSONRes, *idx, *ret = NULL;
+
+	JSONEndp = malloc(strlen(sURL) + 45);
+	if (!JSONEndp)
+		exit(-1);
+
+	strcpy(JSONEndp, MTO_API);
+	strcat(JSONEndp, sURL);
+
+	JSONRes = cUrlPerform(JSONEndp, NULL);
+	if (!JSONRes)
+		goto cleanup;
+
+	idx = strstr(JSONRes, "\"last\":{\"datetime\":\"");
+	if (idx == NULL)
+		goto cleanup;
+
+	idx = strstr(idx, "\"uri\":[\"");
+	ret = &idx[8];
+	idx = MIN_PTR(strchr(idx, ','), strstr(idx, "\"]"));
+	idx[0] = '\0';
+	ret = strdup(ret);
+
+cleanup:
 	free(JSONRes);
 
-	return PermLnk;
+	return ret;
 }
 
-/*
- * Append _perma to filename
- */
-char *fNameGetPerma(char* fName) {
+char *getPermaLink(char *sURL)
+{
+	char *JSONReq, *JSONRes, *idx, *ret = NULL;
 
-	char *r = malloc(strlen(fName) + 7);
-	strcpy(r, fName);
-	char *ext = strstr(r, ".json");
+	JSONReq = malloc(strlen(sURL) + 32);
+	if (!JSONReq)
+		exit(-1);
 
-	if (ext)
-	{
-		strcpy(ext, "_perma");
-		strcat(ext, ".json");
-	} else {
-		printf("Something went terribly wrong!\n");
-	}
+	sprintf(JSONReq, "{\n\t\"url\": \"%s\",\n\t\"title\": \"\"\n}\n", sURL);
 
-	return r;
+	JSONRes = cUrlPerform(PRM_API, JSONReq);
+	if (!JSONRes)
+		goto cleanup;
+
+	idx = strstr(JSONRes, "\"guid\": \"");
+	if (idx == NULL)
+		goto cleanup;
+	idx[18] = '\0';
+
+	ret = malloc(32);
+	if (!ret)
+		exit(-1);
+
+	strcpy(ret, "http://perma.cc/");
+	strcat(ret, &idx[9]);
+
+cleanup:
+	free(JSONRes);
+
+	return ret;
 }
 
-/*
- * Handle pdf file, this is VERY HACKY!
- */
-void pdf2perma(char *fNameIn)
+char *getPermLink(char *sURL)
+{
+	char *ret;
+
+	ret = getMemtoLink(sURL);
+	if (ret == NULL)
+		ret = getPermaLink(sURL);
+
+	return ret;
+}
+
+void pdf2perma(char *sNameInPdf)
 {
 	int e;
-	char *cmd = malloc(11 + strlen(fNameIn));
+	FILE *fp;
+	char *cmd;
+	char *cur;
+	char *end;
+	char *sIn;
+	char *sNameInTxt;
+	char *sNameOutCsv;
+	long iInLen;
 
-	strcpy(cmd, "pdftotext ");
-	strcat(cmd, fNameIn);
+	cmd = malloc(11 + strlen(sNameInPdf));
+	sprintf(cmd, "pdftotext %s", sNameInPdf);
 
 	e = system(cmd);
 	free(cmd);
 
-	if (e) {
-		printf("E: Could not convert pdf to text :(\n");
+	if (e)
+	{
+		printf("E: Could not convert %s :(\n", sNameInPdf);
 		return;
 	}
 
-	char *fNameInTxt = strdup(fNameIn);
-	char *fNameInExt = strstr(fNameInTxt, ".pdf");
-	strcpy(fNameInExt, ".txt");
+	sNameInTxt = strdup(sNameInPdf);
+	strcpy(&sNameInTxt[strlen(sNameInPdf) - 3], "txt");
+	sNameOutCsv = strdup(sNameInPdf);
+	strcpy(&sNameOutCsv[strlen(sNameInPdf) - 3], "csv");
 
-	FILE *fpIn = fopen(fNameInTxt, "r");
-	unsigned fSizeIn;
-	char *pStrIn;
+	fp = fopen(sNameInTxt, "r");
+	fseek(fp, 0, SEEK_END);
+	iInLen = ftell(fp);
+	sIn = malloc(iInLen + 1);
+	rewind(fp);
+	fread(sIn, sizeof(char), (size_t) iInLen, fp);
+	fclose(fp);
+	free(sNameInTxt);
 
-	fseek(fpIn, 0, SEEK_END);
-	fSizeIn = ftell(fpIn);
-	rewind(fpIn);
-	pStrIn = malloc(fSizeIn * sizeof(char));
-	fread(pStrIn, sizeof(char), fSizeIn, fpIn);
-	fclose(fpIn);
-	fpIn = fopen(fNameInTxt, "w");
+	fp = fopen(sNameOutCsv, "w+");
 
-	char *cur = pStrIn, *end = pStrIn, *wrh = pStrIn;
+	cur = sIn;
 
 	while(cur = strstr(cur, "http"))
 	{
-		if (cur[4] == 's'
-		|| (cur[4] == ':'))
+		if (cur[4] == ':'
+		|| (cur[4] == 's' && cur[5] == ':'))
 		{
-			end = strchr(cur+1, ' ');
-			end = MIN(strchr(cur, ';'), end);
-			end = MIN(strchr(cur, '['), end);
-			end = MIN(strstr(cur+1, "http"), end);
-			end = MIN(strstr(cur, ".\n"), end);
-			end = MIN(strstr(cur, ". "), end);
-			end = MIN(strstr(cur, "\n\n"), end);
+			long  i = 0;
+			char *cURL, *tURL;
+
+			end = NULL;
+			end = MIN_PTR(end, strchr(cur, ' '));
+			end = MIN_PTR(end, strchr(cur, ';'));
+			end = MIN_PTR(end, strchr(cur, '['));
+			end = MIN_PTR(end, strstr(cur+1, "http"));
+			end = MIN_PTR(end, strstr(cur, ".\n"));
+			end = MIN_PTR(end, strstr(cur, ". "));
+			end = MIN_PTR(end, strstr(cur, "\n\n"));
 
 			if (end[-1] == '.')
-			{
 				end--;
-			}
-
 			end[0] = '\0';
-			fprintf(fpIn, "%s ", wrh);
-			char *cURL = strdup(cur);
-			char *tURL = cURL;
-			int i = 0;
+
+			cURL = strdup(cur);
+			tURL = cURL;
 			while (*tURL)
 			{
+				if (*tURL == '\\' && tURL[1] == 'n')
+					 tURL ++; else
 				if (*tURL != ' '
-				&& (*tURL != '\n')
-				&& (!(tURL[0] == '\\' && tURL[1] == 'n'))
-				&& (!(tURL[0] == 'n' && tURL[-1] == '\\'))
-				)
-					cURL[i++] = *tURL;
+				&& (*tURL != '\n'))
+					 cURL[i++] = *tURL;
 				tURL++;
 			}
 			cURL[i] = '\0';
-			printf("%s\n", cURL);
-			char *pLink = getPermaLink(cURL);
+
+			fprintf(fp, "%s,", cURL);
+			tURL = getPermLink(cURL);
+			if (tURL == NULL)
+				tURL = strdup("E: API");
+			fprintf(fp, "%s\n", tURL);
+			free(tURL);
 			free(cURL);
-			end[0] = ' ';
-			if (pLink) {
-				fprintf(fpIn, "(http://perma.cc/%s)", pLink);
-				free(pLink);
-			}
-			cur++;
-			wrh = end;
+
+			cur = end + 1;
 		}
 	}
-	free(pStrIn);
-	fclose(fpIn);
+
+	free(sIn);
+	fclose(fp);
+	free(sNameOutCsv);
 }
 
 /*
  * This function handles json files, this is VERY HACKY!
+ * Most of it deals with the **bad** links in legal text
  *
- * Most of it deals with the idiotic links in legal text
+ * Also, this function is as is from the hackathon version
+ * as I have finals coming up and cannot afford to deal
+ * with this at the moment.
  *
- * Hall of Fame (in no specific order (split at whitespace, some extra parts
- *				preserved for user to understand that I'm not a psycho):
+ * Hall of Fame:
+ *
  *	http://www.whitehouse drugpolicy.gov/streetterms/ByType.asp?int TypeID =2
  *	Cocaine,\nhttp://www.whitehousedrugpolicy.gov/streetterms/ByType.asp?intTyp\neID=2
  *	http://www.usdoj.gov/oig/special/s 0608/full&#8212;report.pdf.</p>\n
@@ -214,16 +274,18 @@ void pdf2perma(char *fNameIn)
  *	\"\nhttp://www.epa.gov/ogwdw/contaminants/dw_contamfs/benzene.html\n(last
  */
 
-void web2perma(char *fNameIn)
+void jso2perma(char *fNameIn)
 {
-	char *pdf = strstr(fNameIn, ".pdf");
-
-	if (pdf && pdf[4] == '\0')
-		return pdf2perma(fNameIn);
-
-	char *pStrIn, *fNameOut = fNameGetPerma(fNameIn);
+	char *pStrIn, *fNameOut;
 	unsigned fSizeIn;
-	FILE *fpIn = fopen(fNameIn, "r"), *fpOut = fopen(fNameOut, "w+");
+	FILE *fpIn, *fpOut;
+
+	fNameOut = malloc(strlen(fNameIn) + 7);
+	strcpy(fNameOut, fNameIn);
+	strcpy(&fNameOut[strlen(fNameOut) - 5], "_perma.json");
+
+	fpIn  = fopen(fNameIn, "r");
+	fpOut = fopen(fNameOut, "w+");
 
 	if (!fpIn)
 		printf("E: Could not open input file %s\n", fNameIn);
@@ -253,21 +315,21 @@ void web2perma(char *fNameIn)
 		||  (cur[4] == ':'))
 		{
 			end = strchr(cur, '<');
-			end = MIN(strchr(cur, '>'), end);
-			end = MIN(strchr(cur, '%'), end);
-			end = MIN(strchr(cur, '|'), end);
-			end = MIN(strchr(cur, '^'), end);
-			end = MIN(strchr(cur, '['), end);
+			end = MIN_PTR(strchr(cur, '>'), end);
+			end = MIN_PTR(strchr(cur, '%'), end);
+			end = MIN_PTR(strchr(cur, '|'), end);
+			end = MIN_PTR(strchr(cur, '^'), end);
+			end = MIN_PTR(strchr(cur, '['), end);
 			char *sc = strchr(cur, ';');
 			char *am = strchr(cur, '&');
 			if (am > sc)
-				end = MIN(sc, end);
-			end = MIN(strchr(cur, '('), end);
-			end = MIN(strchr(cur, ')'), end);
-			end = MIN(strchr(cur, ','), end);
-			end = MIN(strchr(cur, '\''), end);
-			end = MIN(strchr(cur, '\"'), end);
-			end = MIN(strstr(cur+1, "http"), end);
+				end = MIN_PTR(sc, end);
+			end = MIN_PTR(strchr(cur, '('), end);
+			end = MIN_PTR(strchr(cur, ')'), end);
+			end = MIN_PTR(strchr(cur, ','), end);
+			end = MIN_PTR(strchr(cur, '\''), end);
+			end = MIN_PTR(strchr(cur, '\"'), end);
+			end = MIN_PTR(strstr(cur+1, "http"), end);
 
 			char *p = cur;
 			for (;;) {
@@ -317,15 +379,12 @@ void web2perma(char *fNameIn)
 					strcpy(p+1, p+4);
 				}
 			}
-			printf("%s\n", cURL);
-			char *pURL = getPermaLink(cURL);
+			char *pURL = getPermLink(cURL);
 			free(cURL);
 			if (pURL)
 			{
-				fprintf(fpOut, " (http://perma.cc/%s)", pURL);
+				fprintf(fpOut, " (%s)", pURL);
 				free(pURL);
-			} else {
-				fprintf(fpOut, cur);  // I'm not really sure WTF I was doing, fix this later.
 			}
 			end[0] = t;
 
@@ -343,7 +402,28 @@ void web2perma(char *fNameIn)
 
 int main(int argc, char *argv[])
 {
-	for (int i = 1; i < argc; i += 2)
-		web2perma(argv[i]);
+	curl_global_init(CURL_GLOBAL_ALL);
+
+	for (int i = 1; i < argc; i++)
+	{
+		char *pptr = strstr(argv[i], ".pdf");
+		char *jptr = strstr(argv[i], ".json");
+
+		while (pptr != NULL && pptr[4] != '\0')
+			pptr = strstr(pptr+4, ".pdf");
+		while (jptr != NULL && jptr[5] != '\0')
+			jptr = strstr(jptr+5, ".json");
+
+		if (pptr == NULL && jptr == NULL)
+		{
+			printf("Usage: ./url2perma [*.pdf|*.json]+\n");
+		} else if (pptr == NULL && jptr != NULL) {
+			jso2perma(argv[i]);
+		} else if (pptr != NULL && jptr == NULL) {
+			pdf2perma(argv[i]);
+		} // else Impossibru!
+	}
+
+	curl_global_cleanup();
 }
 
